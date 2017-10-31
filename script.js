b7fa9e24645fbed3f7e4c4b8cd96a1c3146661fc
reymcl22 <reymcl22@gmail.com>
2017-10-30 21:10:29 -0500

    archivos

---
+++ script.js
@@ -0,0 +1,14 @@
+function openPresent(event) {
+  const newHeader = document.createElement('h1');
+  newHeader.textContent = 'Te amo mi Milagros, amor mio!';
+  const newImage = document.createElement('img');
+  newImage.src = 'corazon.gif';
+  
+  const container = document.querySelector('#container');
+  container.innerHTML = '';
+  container.appendChild(newHeader);
+  container.appendChild(newImage);
+}
+
+const image = document.querySelector('img');
+image.addEventListener('click', openPresent);
